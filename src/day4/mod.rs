d6dee6cce48bf5c298866d697364de1b7c324398
Samuel Trew <samuel.trew@outlook.com>
2022-12-04 10:07:35 +0000

    Finished day 4

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -4,10 +4,46 @@ pub struct Day4 {}
 
 impl Problem for Day4 {
     fn part_one(&self, input: &str) -> String {
-        return format!("Part 1 not implemented.");
+        let pairs = get_pairs(input);
+
+        let count = pairs
+            .map(|p| {
+                let left_contains = p.0.0 <= p.1.0 && p.0.1 >= p.1.1; // 25-87,26-55 => true
+                let right_contains = p.0.0 >= p.1.0 && p.0.1 <= p.1.1; // 26-55,25-87 => true
+
+                return (left_contains || right_contains) as i32;
+            })
+            .sum::<i32>();
+
+        return format!("{}", count);
     }
 
     fn part_two(&self, input: &str) -> String {
-        return format!("Part 1 not implemented.");
+        let pairs = get_pairs(input);
+
+        let count = pairs
+            .map(|p| {
+                let r_overlap = p.1.0 <= p.0.1 && p.1.1 >= p.0.0;
+                let l_overlap = p.0.0 <= p.1.1 && p.0.1 >= p.1.0;
+
+                return (r_overlap || l_overlap) as i32;
+            })
+            .sum::<i32>();
+
+        return format!("{}", count);
     }
+}
+
+fn get_pairs(input: &str) -> impl Iterator<Item = ((i32, i32), (i32, i32))> + '_ {
+    return input
+        .split("\n")
+        .map(|elf_pair| elf_pair
+            .split(&['-', ',', '\r'][..]) // 25-87,25-25 => [25, 87, 25, 25]
+        )
+        .map(|mut pair|
+            (
+                (pair.next().unwrap().parse::<i32>().unwrap(), pair.next().unwrap().parse::<i32>().unwrap()),
+                (pair.next().unwrap().parse::<i32>().unwrap(), pair.next().unwrap().parse::<i32>().unwrap()),
+            )
+        );
 }
--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 mod problem;
 
 fn day_to_problem(day: u8) -> Option<Box<dyn problem::Problem>> {
@@ -19,6 +20,7 @@ fn day_to_problem(day: u8) -> Option<Box<dyn problem::Problem>> {
         2 => Some(Box::new(day2::Day2 {})),
         3 => Some(Box::new(day3::Day3 {})),
         4 => Some(Box::new(day4::Day4 {})),
+        5 => Some(Box::new(day5::Day5 {})),
         // ...
         _ => None,
     }
