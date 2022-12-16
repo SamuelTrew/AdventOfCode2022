71fcc81ea7b63e8451bd78bef8e84a45b58cc18a
Samuel Trew <samuel.trew@outlook.com>
2022-12-16 17:27:43 +0100

    Fixed day 12 part1 and refactored loop

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::collections::{VecDeque};
 
 use crate::problem::Problem;
 
@@ -10,13 +10,13 @@ impl Problem for Day12 {
         let grid = input
             .lines()
             .enumerate()
-            .map(|(i, l)| l
+            .map(|(y, l)| l
                 .chars()
                 .filter(|chr| *chr != '\r')
                 .enumerate()
-                .map(|(j, chr)| {
-                    if chr == 'S' { start = (i, j);  return 0; }
-                    if chr == 'E' { return 26; }
+                .map(|(x, chr)| {
+                    if chr == 'S' { start = (x, y);  return 0; }
+                    if chr == 'E' { return 1000; }
                     return chr as i32 - 97;
                 }) // Turn to numbers
                 .collect::<Vec<i32>>())
@@ -39,101 +39,52 @@ impl Problem for Day12 {
 // This means that it has taken longer to reach that point than the current iteration
 // If they are None then we haven't visited and we continue
 fn bfs (grid: &Vec<Vec<i32>>, result: &mut Vec<Vec<Option<i32>>>, point: (usize, usize)) {
-    let mut queue = VecDeque::<(usize, usize)>::from([point]);
-    let mut visited = 0;
-    let mut max_depth = 0;
+    let mut queue = VecDeque::from([point]);
 
     while let Some((x, y)) = queue.pop_front() {
-        visited += 1;
         let curr_depth = grid[y][x];
         let curr_result = result[y][x].unwrap();
 
-        if max_depth < curr_depth { max_depth = curr_depth; }
-
         // Left
         if x != 0 {
-            let left_depth = grid[y][x-1];
-            let left_result = result[y][x-1];
-            let can_go_left =
-                // 1 higher level or same or lower
-                left_depth <= curr_depth + 1 &&
-                // Left hasn't been visited or left result took longer to reach that point than the current iteration
-                (left_result == None ||
-                left_result.unwrap() > curr_result + 1);
-
-            if can_go_left {
-                if left_result != None {
-                    println!("left{}", left_result.unwrap() - curr_result + 1);
-                }
-                result[y][x-1] = Some(curr_result + 1);
-                println!("{:?}:{}", (x, y), curr_result + 1);
-                queue.push_back((x-1, y));
-            }
+            search_in_direction(grid, result, x-1, y, curr_depth, curr_result, &mut queue);
         }
 
         // Right
         if x != grid[0].len()-1 {
-            let right_depth = grid[y][x+1];
-            let right_result = result[y][x+1];
-            let can_go_right =
-                // Check level
-                right_depth <= curr_depth + 1 &&
-                // Check result
-                (right_result == None ||
-                right_result.unwrap() > curr_result + 1);
-
-            if can_go_right {
-                if right_result != None {
-                    println!("right{}", right_result.unwrap() - curr_result + 1);
-                }
-                result[y][x+1] = Some(curr_result + 1);
-                println!("{:?}:{}", (x, y), curr_result + 1);
-                queue.push_back((x+1, y));
-            }
+            search_in_direction(grid, result, x+1, y, curr_depth, curr_result, &mut queue);
         }
 
         // Up
         if y != 0 {
-            let up_depth = grid[y-1][x];
-            let up_result = result[y-1][x];
-            let can_go_up =
-                // Check level
-                up_depth <= curr_depth + 1 &&
-                // Check result
-                (up_result == None ||
-                up_result.unwrap() > curr_result + 1);
-
-            if can_go_up {
-                if up_result != None {
-                    println!("up{}", up_result.unwrap() - curr_result + 1);
-                }
-                result[y-1][x] = Some(curr_result + 1);
-                println!("{:?}:{}", (x, y), curr_result + 1);
-                queue.push_back((x, y-1));
-            }
+            search_in_direction(grid, result, x, y-1, curr_depth, curr_result, &mut queue)
         }
 
         // Down
         if y != grid.len()-1 {
-            let down_depth = grid[y+1][x];
-            let down_result = result[y+1][x];
-            let can_go_down =
-                // Check level
-                down_depth <= curr_depth + 1 &&
-                // Check result
-                (down_result == None ||
-                down_result.unwrap() > curr_result + 1);
-
-            if can_go_down {
-                if down_result != None {
-                    println!("down{}", down_result.unwrap() - curr_result + 1);
-                }
-                result[y+1][x] = Some(curr_result + 1);
-                println!("{:?}:{}", (x, y), curr_result + 1);
-                queue.push_back((x, y+1));
-            }
+            search_in_direction(grid, result, x, y+1, curr_depth, curr_result, &mut queue)
         }
     }
-    println!("{}", visited);
-    println!("{}", max_depth);
+}
+
+fn search_in_direction(grid: &Vec<Vec<i32>>, result: &mut Vec<Vec<Option<i32>>>, x: usize, y: usize, curr_depth: i32, curr_result: i32, queue: &mut VecDeque<(usize, usize)>) {
+    let depth = grid[y][x];
+
+    if depth == 1000 { // E
+        result[y][x] = Some(curr_result + 1);
+        return;
+    }
+
+    let res = result[y][x];
+    let can_go =
+        // 1 higher level or same or lower
+        depth <= curr_depth + 1 &&
+        // Left hasn't been visited or left result took longer to reach that point than the current iteration
+        (res == None ||
+        res.unwrap() > curr_result + 1);
+
+    if can_go {
+        result[y][x] = Some(curr_result + 1);
+        queue.push_back((x, y));
+    }
 }
